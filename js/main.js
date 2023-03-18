ba4b77f501430b782f33ff6060222ba27fb78650
lekha450 <127828355+lekha450@users.noreply.github.com>
2023-03-18 17:56:53 +0530

    update6

--- js/main.js
+++ js/main.js
@@ -139,7 +139,7 @@ function myFunction(x) {
 var x = window.matchMedia("(max-width: 700px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper",{
   slidesPerView: slide_count,
   spaceBetween: 30,
   smartSpeed: 250,
